9d3fd9b54102e270a4eafde317c1dcd74ce62257
Guillaume Brosse <guillaume@mh-communication.fr>
2020-10-06 15:59:26 +0200

    Remove unused layer model.

--- frontend/app/models/geocms/layer.rb
+++
@@ -1,9 +0,0 @@
-module Geocms
-  class Layer
-    # pg_search_scope :search, against: [:name, :title, :description],
-    #               :using => {
-    #                 :tsearch => {:dictionary => "french"}
-    #               },
-    #               :ignoring => :accents
-  end
-end
