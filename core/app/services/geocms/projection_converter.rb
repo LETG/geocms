19ba1894fdd5ea39172fbba7fde7b7bf234aa9da
Guillaume Brosse <guillaume@mh-communication.fr>
2020-10-08 10:44:02 +0200

    Uncomment projection converter constant.

--- core/app/services/geocms/projection_converter.rb
+++ core/app/services/geocms/projection_converter.rb
@@ -1,12 +1,12 @@
 module Geocms
   class ProjectionConverter
 
-    # EPSG_2154 = RGeo::CoordSys::Proj4.new("+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
-    # EPSG_4326 = RGeo::CoordSys::Proj4.new("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+    EPSG_2154 = RGeo::CoordSys::Proj4.new("+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+    EPSG_4326 = RGeo::CoordSys::Proj4.new("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
 
-    # CRS_84 = RGeo::CoordSys::Proj4.new("+proj=longlat +datum=WGS84 +no_defs ")
+    CRS_84 = RGeo::CoordSys::Proj4.new("+proj=longlat +datum=WGS84 +no_defs ")
 
-    # EPSG_3857 = RGeo::CoordSys::Proj4.new("+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+    EPSG_3857 = RGeo::CoordSys::Proj4.new("+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
     # from CRS 84
     def initialize(proj, point = [])
       @new_proj = proj
