b0339924b4a31861709f31a842ddf500865aa821
Guillaume Brosse <guillaume@mh-communication.fr>
2020-11-19 12:14:42 +0100

    Fix image issue for default logo.

--- frontend/app/helpers/geocms/application_helper.rb
+++ frontend/app/helpers/geocms/application_helper.rb
@@ -1,8 +1,12 @@
 module Geocms
   module ApplicationHelper
     def logo_for_tenant(tenant = current_tenant)
-      url = (tenant && tenant.logo?) ? tenant.logo.url : "dotgeocms.png"
-      image_tag url
+      	url = (tenant && tenant.logo?) ? tenant.logo.url : "geocms/dotgeocms.png"
+      	image_tag url
     end
+
+    def logo_url_for_tenant(tenant = current_tenant) 
+   	 	url = (tenant && tenant.logo?) ? tenant.logo.url : "geocms/dotgeocms.png"
+	end
   end
 end
