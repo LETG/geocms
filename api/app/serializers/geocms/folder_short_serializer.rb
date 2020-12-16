76387a998c3265f197336fdb0bc2b2166fc9db53
Guillaume Brosse <guillaume@mh-communication.fr>
2020-12-16 10:45:21 +0100

    Add has_many to FolderShortSerializer. Fix issue where projects weren't in folders.

--- api/app/serializers/geocms/folder_short_serializer.rb
+++ api/app/serializers/geocms/folder_short_serializer.rb
@@ -1,5 +1,7 @@
 module Geocms
   class FolderShortSerializer < ActiveModel::Serializer
     attributes :id, :slug, :name, :visibility
+
+    has_many :contexts, embed: :objects, serializer: ContextShortSerializer
   end
 end
