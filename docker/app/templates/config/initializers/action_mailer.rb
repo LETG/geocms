204679f7dab3765a0924380257f9cb23bd206c2c
Guillaume Brosse <guillaume@mh-communication.fr>
2022-10-14 11:05:23 +0200

    Update configuration and env for mail.
    Maildev does not support empty string as username/password.

--- docker/app/templates/config/initializers/action_mailer.rb
+++ docker/app/templates/config/initializers/action_mailer.rb
@@ -8,9 +8,11 @@ Rails.application.configure do
     address: ENV.fetch("SMTP_ADDRESS", ""),
     port: ENV.fetch("SMTP_PORT", ""),
     domain: ENV.fetch("SMTP_DOMAIN", ""),
-    user_name: ENV.fetch("SMTP_USER", ""),
-    password: ENV.fetch("SMTP_PASSWORD", ""),
-    authentication: "login",
-    enable_starttls_auto: true
+    user_name: ENV.fetch("SMTP_USER", "").present? ? ENV.fetch("SMTP_USER") : nil,
+    password: ENV.fetch("SMTP_PASSWORD", "").present? ? ENV.fetch("SMTP_PASSWORD") : nil,
+    authentication: ENV.fetch("SMTP_AUTHENTICATION", "").present? ? ENV.fetch("SMTP_AUTHENTICATION") : nil,
+    enable_starttls_auto: ActiveModel::Type::Boolean.new.cast(ENV.fetch("SMTP_ENABLE_STARTTLS_AUTO", "")),
+    ssl: ActiveModel::Type::Boolean.new.cast(ENV.fetch("SMTP_SSL", "")),
+    tls: ActiveModel::Type::Boolean.new.cast(ENV.fetch("SMTP_TLS", "")),
   }
 end
