d1838ea5495c463306dde9f0306b218b7f9b1057
Mathias Rouan <Mathias.Rouan@univ-brest.fr>
2023-12-11 12:05:42 +0100

    add default style in thumbnail

--- common_geocms_dependencies.rb
+++ common_geocms_dependencies.rb
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 gem 'pg', '>= 0.18', '< 2.0'
 
 # gem 'ruby-ogc', :git => 'https://github.com/LETG/ruby-ogc.git'
-gem 'ruby-ogc', :path => '/ruby-ogc'
+gem 'ruby-ogc', :path => '../ruby-ogc'
 gem 'responders'
 
 #gem 'sass-rails',   '~> 4.0.2'
