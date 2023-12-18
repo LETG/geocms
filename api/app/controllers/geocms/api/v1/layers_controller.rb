87a82979cf6d3052ab305ef5e363d7a6dd13ba19
Guillaume Brosse <guillaume@mh-communication.fr>
2023-12-18 10:18:27 +0100

    Fix issue with select layers

--- api/app/controllers/geocms/api/v1/layers_controller.rb
+++ api/app/controllers/geocms/api/v1/layers_controller.rb
@@ -25,7 +25,10 @@ module Geocms
           @layers = Geocms::Layer.joins(:categories).where("geocms_categories.account_id= ?",current_tenant.id).search(params[:q])
           @categories = Geocms::Category.where("geocms_categories.account_id= ?",current_tenant.id).search(params[:q])
 
-          respond_with layers: @layers.to_a.uniq, categories: @categories.to_a.uniq, layer_serializer: Geocms::LayerSerializer, category_serializer: Geocms::CategorySerializer
+          layers_data = @layers.to_a.uniq.map { |layer| Geocms::LayerSerializer.new(layer).serializable_hash }
+          categories_data = @categories.to_a.uniq.map { |category| Geocms::CategorySerializer.new(category).serializable_hash }
+          
+          respond_with layers: layers_data, categories: categories_data
         end
 
         def bbox
