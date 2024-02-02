493d6903fd2cf1581f5e90fdf99c216ff60b7ac6
Guillaume Brosse <guillaume@mh-communication.fr>
2024-02-02 15:57:28 +0100

    Fix issue to open folder list if user is not logged in

--- api/app/serializers/geocms/folder_short_serializer.rb
+++ api/app/serializers/geocms/folder_short_serializer.rb
@@ -5,7 +5,8 @@ module Geocms
     has_many :contexts, embed: :objects, serializer: ContextShortSerializer
 
     def personal
-      scope.id == object.user_id
+      # If scope is defined, we check if folder user id is the same as the current_user. This allow to add property "personal" to indicate if folder belongs to the user
+      scope.present? ? scope.id == object.user_id : false
     end
   end
 end
