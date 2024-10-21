77db68643e3265a8ffba1eeefadde2cd867d1d54
Simon Ybert <simon@imagile.fr>
2024-10-21 09:55:43 +0200

    fix(auth): email/password authentication

--- frontend/app/controllers/geocms/sessions_controller.rb
+++ frontend/app/controllers/geocms/sessions_controller.rb
@@ -9,14 +9,6 @@ module Geocms
     end
 
     def create
-      user = User.from_omniauth(request.env['omniauth.auth'])
-      if user
-        session[:user_id] = user.id
-        redirect_to root_url, success: t("session.logged_in")
-      else
-        redirect_to root_url, alert: t("session.invalid_credentials")
-      end
-=begin
       user = login(params[:username], params[:password], params[:remember_me])
 
       if user
@@ -29,7 +21,6 @@ module Geocms
       else
         redirect_to login_path, :flash => { :error => t("session.invalid_credentials") }
       end
-=end
     end
 
     def destroy
