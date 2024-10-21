940574519d4014da4bdf3d802634a8b216983720
Simon Ybert <simon@imagile.fr>
2024-10-21 09:55:06 +0200

    chore(auth): remove unused concern

--- frontend/app/controllers/geocms/application_controller.rb
+++ frontend/app/controllers/geocms/application_controller.rb
@@ -1,6 +1,5 @@
 module Geocms
   class ApplicationController < ActionController::Base
-    use OmniAuth::Strategies::Developer
     set_current_tenant_by_subdomain(Geocms::Account, :subdomain)
 
     respond_to :html
