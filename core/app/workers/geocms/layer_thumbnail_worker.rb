84fe101041fa7fe2e9ee3ad95fe89a57b6832a0f
Mathias Rouan <Mathias.Rouan@univ-brest.fr>
2024-12-27 12:22:06 +0100

    add remote_thumbnail_url in log for debug

--- core/app/workers/geocms/layer_thumbnail_worker.rb
+++ core/app/workers/geocms/layer_thumbnail_worker.rb
@@ -5,6 +5,7 @@ module Geocms
     def perform(layer_id, data_source, crs, bbox)
       layer = Geocms::Layer.find layer_id
       remote_thumbnail_url = ROGC::WMSClient.get_map(data_source, layer.name, layer.default_style, bbox, 64, 64, crs)
+      logger.info "remote url : #{remote_thumbnail_url}"
       layer.remote_thumbnail_url = ROGC::WMSClient.get_map(data_source, layer.name, layer.default_style, bbox, 64, 64, crs)  
       layer.skip_get_thumbnail = true
       layer.save!
